bb3dea49e44e8e61754fcf83d9fbad2fb912aafc
sunghyun <cktjdgus0228@naver.com>
2025-07-28 15:54:23 +0900

    d

--- siseon-backend/src/main/java/siseon/backend/domain/Profile.java
+++ siseon-backend/src/main/java/siseon/backend/domain/Profile.java
@@ -1,12 +1,15 @@
 package siseon.backend.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import siseon.backend.config.JsonConverter;
-import java.util.Map;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Entity
 @Getter
@@ -20,15 +23,31 @@ public class Profile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Column(nullable = false)
     private String name;
+
+    @NotNull
+    @Column(nullable = false)
     private LocalDate birthDate;
+
+    @NotNull
+    @Column(nullable = false)
     private Float height;
+
+    @NotNull
+    @Column(nullable = false, name = "left_vision")
     private Float leftVision;
+
+    @NotNull
+    @Column(nullable = false, name = "right_vision")
     private Float rightVision;
+
     private String imageUrl;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Lob
@@ -36,7 +55,13 @@ public class Profile {
     @Column(columnDefinition = "TEXT")
     private Map<String, Object> settings;
 
+    @OneToMany(mappedBy = "profile", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Preset> presets = new ArrayList<>();
+
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
     @PrePersist
